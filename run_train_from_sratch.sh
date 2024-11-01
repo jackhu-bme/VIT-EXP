95731325d1dc039313d235de7c28f2e7a48d6723
jackhu-bme <2368907635@sjtu.edu.cn>
2024-11-01 21:50:50 +0000

    use new torch and trans lib, start hpc 2 exp

--- run_train_from_sratch.sh
+++ run_train_from_sratch.sh
@@ -1,3 +1,3 @@
-# accelerate launch --use_fsdp scripts/run_train.py --config ct_clip_ori_hpc_1.yaml
-accelerate launch scripts/run_train.py --config ct_clip_ori_hpc_1.yaml
+accelerate launch --use_fsdp scripts/run_train.py --config ct_clip_ori_hpc_2.yaml
+# accelerate launch scripts/run_train.py --config ct_clip_ori_hpc_1.yaml
 
