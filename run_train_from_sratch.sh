e6b33bf04db4532bfea5b402a150336874d94d58
jackhu-bme <2368907635@sjtu.edu.cn>
2024-11-06 14:23:30 +0000

    add the git ignore for demo testing / evaluation

--- run_train_from_sratch.sh
+++ run_train_from_sratch.sh
@@ -3,4 +3,4 @@ accelerate launch scripts/run_train.py --config ct_clip_ori_hpc_2.yaml
 
 
 # resume training
-accelerate launch scripts/run_train_resume.py --config ct_clip_ori_hpc_2.yaml --resume ../ckpts/CTClip.60500.pt
+accelerate launch scripts/run_train_resume.py --config ct_clip_ori_hpc_2.yaml --resume ../ckpts/CTClip.80500.pt
