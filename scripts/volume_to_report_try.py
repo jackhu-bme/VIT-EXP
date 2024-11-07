d39e44ab5fdb3e67918ee301192e7cb3492f775a
jackhu-bme <2368907635@sjtu.edu.cn>
2024-11-07 08:01:01 +0000

    add volume to report retrieval

---
+++ scripts/volume_to_report_try.py
@@ -0,0 +1,102 @@
+import numpy as np
+import torch
+import tqdm
+import os
+
+def find_top_k_indices(values, k):
+    # Check if the list has at least 50 values
+    if len(values) < k:
+        raise ValueError(f"The list must contain at least {k} values")
+
+    # Use a combination of 'sorted' and 'enumerate' to sort the values while keeping track of indices
+    sorted_values_with_indices = sorted(enumerate(values), key=lambda x: x[1], reverse=True)
+
+    # Extract the indices of the top k values
+    top_k_indices = [index for index, value in sorted_values_with_indices[:k]]
+
+    return top_k_indices
+
+data_folder = "../../results_volume_to_report/"
+os.makedirs(data_folder, exist_ok=True)
+
+# Scan the folder for image and text .npz files
+
+
+# todo: currently implement this by reversing the loading of code from report2volume_new.py, but ugly
+# need to change variable names in the future
+
+image_npz_files = [f for f in os.listdir("../../valid_latents/text") if f.endswith('.npz')]
+# sort
+image_npz_files = sorted(image_npz_files, key=lambda x: x.split(".")[0].split("_")[-1])
+text_npz_files = [f for f in os.listdir("../../valid_latents/image") if f.endswith('.npz')]
+# sort
+text_npz_files = sorted(text_npz_files, key=lambda x: x.split(".")[0].split("_")[-1])
+
+# Initialize lists to store loaded data
+image_data_list = []
+text_data_list = []
+
+# Load image and text .npz files
+for npz_file in tqdm.tqdm(image_npz_files):
+    file_path = os.path.join("../../valid_latents/image", npz_file)
+    image_data = np.load(file_path)["arr"][0]
+    image_data_list.append(image_data)
+
+for npz_file in tqdm.tqdm(text_npz_files):
+    file_path = os.path.join("../../valid_latents/text", npz_file)
+    text_data = np.load(file_path)["arr"][0]
+    text_data_list.append(text_data)
+
+# Concatenate all loaded image and text data
+image_data = np.array(image_data_list)
+text_data = np.array(text_data_list)
+
+print(image_data.shape)
+
+list_texts = []
+list_ks = [5,10,50,100]
+# list_ks = [5] # for debug only
+for value in tqdm.tqdm(list_ks):
+    num_is_in = 0
+    num_random = 0
+
+    for i in tqdm.tqdm(range(text_data.shape[0])):
+        crosses = []
+        crosses_rands = []
+        for k in range(image_data.shape[0]):
+            text = torch.tensor(text_data[i])
+            image = torch.tensor(image_data[k])
+
+            cross = text @ image
+            crosses.append(cross)
+
+        top_k_indices = find_top_k_indices(crosses, value)
+        if i in top_k_indices:
+            num_is_in += 1
+
+        for k in range(image_data.shape[0]):
+            size = (512)
+            text = torch.rand(size)
+            image = torch.rand(size)
+
+            crosses_rand = text @ image
+            crosses_rands.append(crosses_rand)
+        top_k_indices = find_top_k_indices(crosses_rands, value)
+        if i in top_k_indices:
+            num_random += 1
+
+    clip = num_is_in / text_data.shape[0]
+    rand = num_random / text_data.shape[0]
+    write_str = f"K={value}, clip = {clip}, rand= {rand}"
+    list_texts.append(write_str)
+
+output_file_path = data_folder + f"internal_accessions_i2t_{list_ks[0]}.txt"
+
+# Open the file for writing (you can also use "a" to append if the file already exists)
+with open(output_file_path, "w") as file:
+    # Write each string from the list to the file
+    for string in list_texts:
+        file.write(string + "\n")
+
+# File has been written, close it
+file.close()
