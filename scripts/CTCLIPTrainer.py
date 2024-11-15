84a8dddfdae5cba7da710261ca457d68a4840ac6
jackhu-bme <2368907635@sjtu.edu.cn>
2024-11-15 04:42:52 +0000

    try fix the wandb logging not show in web bug

--- scripts/CTCLIPTrainer.py
+++ scripts/CTCLIPTrainer.py
@@ -329,8 +329,8 @@ class CTClipTrainer(nn.Module):
         # self.print(f"{steps}: loss: {logs['loss']}")
         self.print(f"log: {logs}")
 
-        # self.wandb_logger.log(logs)
-        self.accelerator.log(logs, step=self.steps.int().item())
+        self.wandb_logger.log(logs, step=self.steps.int().item())
+        # self.accelerator.log(logs, step=self.steps.int().item())
 
         # if self.is_main and not (steps % self.save_results_every):
         #     with torch.no_grad():
