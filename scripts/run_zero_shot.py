1c3fa9841e213e4cd49aa377590868e5d52a2840
jackhu-bme <2368907635@sjtu.edu.cn>
2024-11-19 06:46:17 +0000

    change run zero shot path

--- scripts/run_zero_shot.py
+++ scripts/run_zero_shot.py
@@ -1,5 +1,5 @@
 import torch
-from transformer_maskgit import CTViT
+from transformer_maskgit import CTViT, CTViT3D
 from transformers import BertTokenizer, BertModel
 from ct_clip import CTCLIP
 from zero_shot import CTClipInference
@@ -11,31 +11,49 @@ text_encoder = BertModel.from_pretrained("microsoft/BiomedVLP-CXR-BERT-specializ
 text_encoder.resize_token_embeddings(len(tokenizer))
 
 
-image_encoder = CTViT(
-    dim = 512,
-    codebook_size = 8192,
-    image_size = 480,
-    patch_size = 20,
-    temporal_patch_size = 10,
-    spatial_depth = 4,
-    temporal_depth = 4,
-    dim_head = 32,
-    heads = 8
-)
+import yaml
+config = yaml.load(open("../config/train_from_scratch/ct_clip_vit_hpc_v3_1.yaml", "r"), Loader=yaml.FullLoader)
+
+image_encoder = CTViT3D(
+            # dim = 512,
+            dim = config.get("CTViT3D_dim", 768),
+            # codebook_size = 8192,
+            image_size = config.get("image_size", 480),
+            patch_size = config.get("patch_size", 20),
+            temporal_size= config.get("temporal_size", 240),
+            temporal_patch_size= config.get("temporal_patch_size", 10),
+            transformer_blocks = config.get("transformer_blocks", 8),
+            dim_head = config.get("dim_head", 32),
+            heads = config.get("heads", 8),
+            use_flash_attention = config.get("use_flash_attention", True),
+        )
+
+# image_encoder = CTViT(
+#     dim = 512,
+#     codebook_size = 8192,
+#     image_size = 480,
+#     patch_size = 20,
+#     temporal_patch_size = 10,
+#     spatial_depth = 4,
+#     temporal_depth = 4,
+#     dim_head = 32,
+#     heads = 8
+# )
 
 clip = CTCLIP(
+    
     image_encoder = image_encoder,
     text_encoder = text_encoder,
-    dim_image = 294912,
     dim_text = 768,
-    dim_latent = 512,
+    dim_image = 442368,
+    dim_latent = 768,
     extra_latent_projection = False,         # whether to use separate projections for text-to-image vs image-to-text comparisons (CLOOB)
     use_mlm=False,
     downsample_image_embeds = False,
     use_all_token_embeds = False
 )
 
-clip_model_path_for_infer = "../../output_train_scratch_resume/CTClip.100000.pt"
+clip_model_path_for_infer = "../output_train_scratch/train_from_scratch_vit_hpc_1_dim_384/2024-11-16_06-57-22/checkpoints/CTClip.100000.pt"
 clip.load(clip_model_path_for_infer, check=True)
 
 
@@ -45,7 +63,7 @@ inference = CTClipInference(
     reports_file= "/mnt/input/CT-RATE/organized_dataset/csv_dir/reports/validation_reports.csv",
     labels = "/mnt/input/CT-RATE/organized_dataset/csv_dir/labels/valid_predicted_labels.csv",
     batch_size = 1,
-    results_folder="../results_inference_zeroshot_demo/",
+    results_folder="../results_inference_zeroshot_ctvit/",
     num_train_steps = 1,
 )
 
