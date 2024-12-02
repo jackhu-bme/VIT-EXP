b3fc29c6523819bd370b22fd3916489a178443e7
jackhu-bme <2368907635@sjtu.edu.cn>
2024-12-02 20:58:59 +0000

    gpu 1 testing

--- run_zero_shot_cls_multi_gpu.py
+++ run_zero_shot_cls_multi_gpu.py
@@ -4,7 +4,7 @@ import os
 
 CONFIG="configs/train_from_scratch/ct_clip_vit_hpc_v3_1.yaml"
 
-model_dir = "/mnt/input/CT-CLIP-VIT/train_from_scratch_vit_hpc_1_dim_384/2024-11-16_06-57-22/checkpoints/"
+model_dir = "/mnt/input/CT-CLIP-VIT/train_from_scratch_vit_hpc_1_dim_384/2024-11-22_01-06-13/checkpoints"
 
 model_list = os.listdir(model_dir)
 
@@ -33,13 +33,13 @@ end_index = (current_split + 1) * n_models // total_splits
 
 run_models = model_list[start_index:end_index]
 
-current_gpu = 0
+current_gpu = 1
 
 os.environ["CUDA_VISIBLE_DEVICES"] = str(current_gpu)
 
 # MODEL="/mnt/input/CT-CLIP-VIT/train_from_scratch_vit_hpc_1_dim_384/2024-11-22_01-06-13/checkpoints/CTClip.200000.pt"
 
-results_dir = "/mnt/input/CT-CLIP-VIT/results_fast_inference_zeroshot_ctvit_full_multi_gpu"
+results_dir = "/mnt/input/CT-CLIP-VIT/results_fast_inference_zeroshot_ctvit_full_multi_gpu_resume"
 
 for run_model in run_models:
     MODEL = os.path.join(model_dir, run_model)
