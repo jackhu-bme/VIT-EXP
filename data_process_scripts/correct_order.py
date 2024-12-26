8cf5ece12844aaa8db0f6e54efd0ab11e4937e56
jackhu-bme <2368907635@sjtu.edu.cn>
2024-12-26 00:26:35 -0800

    sort

--- data_process_scripts/correct_order.py
+++ data_process_scripts/correct_order.py
@@ -6,7 +6,7 @@ ori_wrong_mask_dir = "../combined_seg_17_cls_revise"
 save_correct_mask_dir = "../train_combined_seg_17_cls_final"
 os.makedirs(save_correct_mask_dir, exist_ok=True)
 
-for mask_name in os.listdir(ori_wrong_mask_dir):
+for mask_name in sorted(os.listdir(ori_wrong_mask_dir)):
     if mask_name.endswith(".npz"):
         mask_file = os.path.join(ori_wrong_mask_dir, mask_name)
         mask_data = np.load(mask_file, allow_pickle=True)["arr_0"]
