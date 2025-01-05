892a47761d6f2c5ca1ff664d7c4daa7997ad41e4
jackhu-bme <2368907635@sjtu.edu.cn>
2025-01-05 18:17:13 +0000

    change exp name

--- run_train_from_sratch.sh
+++ run_train_from_sratch.sh
@@ -1,9 +1,9 @@
-accelerate launch scripts/run_train.py --config ct_clip_vit_seg_hpc_v3_1_div_0_80g_cl_revised_ref.yaml --auto_resume
+accelerate launch scripts/run_train.py --config ct_clip_vit_open_seg_hpc_v2_1_80g.yaml  #--auto_resume
 # accelerate launch scripts/run_train.py --config ct_clip_ori_hpc_1.yaml
 
 # debug
 # python scripts/run_train.py --config ct_clip_vit_open_seg_hpc_v1_1_div_0_80g.yaml --debug
-python scripts/run_train.py --config ct_clip_vit_open_seg_hpc_v2_1_div_0_80g.yaml --debug
+python scripts/run_train.py --config ct_clip_vit_open_seg_hpc_v2_1_80g.yaml --debug
 
 # local 3195 machine debug
 python scripts/run_train.py --config ct_clip_vit_open_seg_v2_1_debug_30_80g.yaml --debug
