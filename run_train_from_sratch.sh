afc476e4cfe913020ec49bd05c44c0afa6897eee
jackhu-bme <2368907635@sjtu.edu.cn>
2025-01-05 02:27:22 +0000

    config the local 3195 debug analysis env

--- run_train_from_sratch.sh
+++ run_train_from_sratch.sh
@@ -5,6 +5,9 @@ accelerate launch scripts/run_train.py --config ct_clip_vit_seg_hpc_v3_1_div_0_8
 # python scripts/run_train.py --config ct_clip_vit_open_seg_hpc_v1_1_div_0_80g.yaml --debug
 python scripts/run_train.py --config ct_clip_vit_open_seg_hpc_v2_1_div_0_80g.yaml --debug
 
+# local 3195 machine debug
+python scripts/run_train.py --config ct_clip_vit_open_seg_v2_1_debug_30_80g.yaml --debug
+
 # resume training
 accelerate launch scripts/run_train.py --config ct_clip_ori_hpc_2.yaml --resume ../ckpts/CTClip.80500.pt
 
