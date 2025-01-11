2d060a490851330075abfdbb2399aa250ebe95a4
jackhu-bme <2368907635@sjtu.edu.cn>
2025-01-11 12:18:52 +0000

    save state in all threads, try again

--- scripts/CTCLIPTrainer.py
+++ scripts/CTCLIPTrainer.py
@@ -738,10 +738,10 @@ class CTClipTrainer(nn.Module):
             # save_policy = FullStateDictConfig(offload_to_cpu=True, rank0_only=True)
             # with FSDP.state_dict_type(self.CTCLIP, StateDictType.FULL_STATE_DICT, save_policy):
             #     state_dict=self.accelerator.get_state_dict(self.CTClip, unwrap=False)
-            if self.is_main:
-                self.print(f"save model at step: {steps}, output_dir: {self.results_folder}")
-                self.accelerator.save_state()
-                print(f"successfully saved model at step: {steps}")
+            # if self.is_main:
+            self.print(f"save model at step: {steps}, output_dir: {self.results_folder}")
+            self.accelerator.save_state()
+            print(f"successfully saved model at step: {steps}")
             # # model_path = str(self.results_folder / f'CTClip.{steps}.pt')
             # # self.accelerator.save(state_dict, model_path)
         self.steps += 1
